a4cd606fab0eec7edff371dd62ff5359195e67ab
Ivan Sued <ivan@practicepanther.com>
2022-07-06 16:25:39 -0700

    Adding logic to use the MouseWorld Static Class

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -16,9 +16,9 @@ public class Unit : MonoBehaviour
             transform.position += moveDirection * (Time.deltaTime * moveSpeed);
         }
 
-        if (Input.GetKeyDown(KeyCode.T))
+        if (Input.GetMouseButtonDown(0))
         {
-            Move(new Vector3(4, 0, 4));
+            Move(MouseWorld.GetPosition());
         }
     }
 
