c6a94cd45a573a8907f21c7a0ce69f65d2acf8a6
Ivan Sued <ivan@practicepanther.com>
2022-07-06 18:36:12 -0700

    Adding parameter logic to play animation

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -3,17 +3,24 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     [SerializeField] private float stoppingDistance = .1f;
+    [SerializeField] private Animator unitAnimator;
 
     private Vector3 _targetPosition;
 
+    private int _walkParameterHash = Animator.StringToHash("IsWalking");
+
     private void Update()
     {
-
         if (Vector3.Distance(_targetPosition, transform.position) > stoppingDistance)
         {
             Vector3 moveDirection = (_targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
             transform.position += moveDirection * (Time.deltaTime * moveSpeed);
+            unitAnimator.SetBool(_walkParameterHash, true);
+        }
+        else
+        {
+            unitAnimator.SetBool(_walkParameterHash, false);
         }
 
         if (Input.GetMouseButtonDown(0))
