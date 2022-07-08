acce57fe767f8b6825c93923a34ebf0e1a4ef6e9
Ivan Sued <ivan@practicepanther.com>
2022-07-08 15:53:16 -0700

    Adding camera movement, rotation and zoom

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,11 +1,36 @@
+using System;
+using Cinemachine;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float rotationSpeed = 100f;
+    [SerializeField] private float zoomAmount = 1f;
+    [SerializeField] private float zoomSpeed = 20f;
+    [SerializeField] private float minZoomFollowOffset = 2f;
+    [SerializeField] private float maxZoomFollowOffset = 12f;
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+
+    private Vector3 _targetFollowOffset;
+    private CinemachineTransposer _cinemachineTransposer;
+
+    private void Start()
+    {
+        _cinemachineTransposer = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+        _targetFollowOffset = _cinemachineTransposer.m_FollowOffset;
+    }
 
     private void Update()
+    {
+        HandleMovement();
+
+        HandleRotation();
+
+        HandleZoom();
+    }
+
+    private void HandleMovement()
     {
         Vector3 inputMoveDirection = new();
 
@@ -13,14 +38,17 @@ public class CameraController : MonoBehaviour
         {
             inputMoveDirection.z += 1f;
         }
+
         if (Input.GetKey(KeyCode.S))
         {
             inputMoveDirection.z -= 1f;
         }
+
         if (Input.GetKey(KeyCode.A))
         {
             inputMoveDirection.x -= 1f;
         }
+
         if (Input.GetKey(KeyCode.D))
         {
             inputMoveDirection.x += 1f;
@@ -28,8 +56,10 @@ public class CameraController : MonoBehaviour
 
         Vector3 moveVector = transform.forward * inputMoveDirection.z + transform.right * inputMoveDirection.x;
         transform.position += moveVector * (moveSpeed * Time.deltaTime);
+    }
 
-
+    private void HandleRotation()
+    {
         Vector3 rotationVector = new();
 
         if (Input.GetKey(KeyCode.Q))
@@ -43,5 +73,22 @@ public class CameraController : MonoBehaviour
         }
 
         transform.eulerAngles += rotationVector * (rotationSpeed * Time.deltaTime);
+    }
+
+    private void HandleZoom()
+    {
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            _targetFollowOffset.y -= zoomAmount;
+        }
+
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            _targetFollowOffset.y += zoomAmount;
+        }
+
+        _targetFollowOffset.y = Mathf.Clamp(_targetFollowOffset.y, minZoomFollowOffset, maxZoomFollowOffset);
+        _cinemachineTransposer.m_FollowOffset = Vector3.Lerp(_cinemachineTransposer.m_FollowOffset, _targetFollowOffset,
+            Time.deltaTime * zoomSpeed);
     }
 }
