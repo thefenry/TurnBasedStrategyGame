0c6b3fc9fd7bcf96aa984dbd589f6a784a902f67
Ivan Sued <ivan@practicepanther.com>
2022-07-08 12:20:59 -0700

    Creating a LevelGrid Manager
    Manages adding unit information to the grid when unit is moved

--- Assets/Scripts/Grid/GridPosition.cs
+++ Assets/Scripts/Grid/GridPosition.cs
@@ -1,4 +1,6 @@
-public readonly struct GridPosition
+using System;
+
+public readonly struct GridPosition: IEquatable<GridPosition>
 {
     public GridPosition(int x, int z)
     {
@@ -13,5 +15,30 @@ public readonly struct GridPosition
     public override string ToString()
     {
         return $"x: {X}; z: {Z}";
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is GridPosition other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Z);
+    }
+
+    public static bool operator ==(GridPosition a, GridPosition b)
+    {
+        return a.X == b.X && a.Z == b.Z;
+    }
+
+    public static bool operator !=(GridPosition a, GridPosition b)
+    {
+        return !(a == b);
+    }
+
+    public bool Equals(GridPosition other)
+    {
+        return this == other;
     }
 }
--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -2,20 +2,17 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
-    [SerializeField] private Transform gridDebugObjectPrefab;
-
-    private GridSystem _gridSystem;
+ 
 
     private void Start()
     {
-        _gridSystem = new GridSystem(10, 10, 2f);
-        _gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
+      
 
-        Debug.Log(new GridPosition(1, 5));
+        //Debug.Log(new GridPosition(1, 5));
     }
 
     private void Update()
     {
-        Debug.Log(_gridSystem.GetGridPosition(MouseWorld.GetPosition()));
+        //Debug.Log(_gridSystem.GetGridPosition(MouseWorld.GetPosition()));
     }
 }
