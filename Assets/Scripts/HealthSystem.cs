4b02caf91a569e7b630ee583525d096b911a8373
Ivan Sued <thefenry@gmail.com>
2022-09-24 14:43:49 -0700

    Adding a unit ui so we can see how much health and actions we have

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -6,6 +6,14 @@ public class HealthSystem : MonoBehaviour
     [SerializeField] private int health = 100;
 
     public event EventHandler OnDeath;
+    public event EventHandler OnHealthChanged;
+
+    private float _maxHealth;
+
+    private void Awake()
+    {
+        _maxHealth = health;
+    }
 
     public void TakeDamage(int damageAmount)
     {
@@ -20,10 +28,14 @@ public class HealthSystem : MonoBehaviour
         {
             Die();
         }
+
+        OnHealthChanged?.Invoke(this, EventArgs.Empty);
     }
 
     private void Die()
     {
         OnDeath?.Invoke(this, EventArgs.Empty);
     }
+
+    public float GetNormalizedHealth() => health / _maxHealth;
 }
